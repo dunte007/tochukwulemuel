b80fea92ed41ca63bdae949570b96c6e7d240d18
Tochukwu Lemuel <54296106+dunte007@users.noreply.github.com>
2019-09-08 00:21:03 +0100

    Updated

--- mail.php
+++ mail.php
@@ -1,14 +1,43 @@
-<?php 
+rmvalidator.addValidation("email","req","Please provide your email");
+frmvalidator.addValidation("email","email",
+  "Please enter a valid email address");
+</script>
+PHP:
 
+<?php
+$errors = '';
+$myemail = 'dunte007@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['Tel']) ||
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
 $name = $_POST['name'];
-$email = $_POST['email'];
-$tel = $_POST['tel'];
+$email_address = $_POST['email'];
 $message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "emailaddress@here.com";
-$subject = "Contact-Form";
-$mailheader = "From: $email \r\n";
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+$to = $myemail;
+$email_subject = "Contact form submission: $name";
+$email_body = "You have received a new message. ".
+" Here are the details:\n Name: $name \n ".
+"Email: $email_address\n Message \n $message";
+$headers = "From: $myemail\n";
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+//redirect to the 'thank you' page
+header('Location: contact-form-thank-you.html');
+
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
-
+}
 ?>
